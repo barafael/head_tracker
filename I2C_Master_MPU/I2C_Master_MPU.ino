c2a63aec56f49138835b788d175c9db9a49ca89c
Rafael Bachmann <mediumendian@gmail.com>
2017-11-13 12:29:33 +0100

    remove superfluous print

--- I2C_Master_MPU/I2C_Master_MPU.ino
+++ I2C_Master_MPU/I2C_Master_MPU.ino
@@ -1,52 +1,3 @@
-/* ============================================
-  I2Cdev device library code is placed under the MIT license
-  Copyright (c) 2012 Jeff Rowberg
-
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files (the "Software"), to deal
-  in the Software without restriction, including without limitation the rights
-  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  copies of the Software, and to permit persons to whom the Software is
-  furnished to do so, subject to the following conditions:
-
-  The above copyright notice and this permission notice shall be included in
-  all copies or substantial portions of the Software.
-
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-  THE SOFTWARE.
-  ===============================================
-*/
-
-/* I2C device class (I2Cdev) demonstration Arduino sketch for MPU6050 class using DMP (MotionApps v2.0)
-  6/21/2012 by Jeff Rowberg <jeff@rowberg.net>
-  Updates should (hopefully) always be available at https://github.com/jrowberg/i2cdevlib
-
-  Changelog:
-      2013-05-08 - added seamless Fastwire support
-                 - added note about gyro calibration
-      2012-06-21 - added note about Arduino 1.0.1 + Leonardo compatibility error
-      2012-06-20 - improved FIFO overflow handling and simplified read process
-      2012-06-19 - completely rearranged DMP initialization code and simplification
-      2012-06-13 - pull gyro and accel data from FIFO packet instead of reading directly
-      2012-06-09 - fix broken FIFO read sequence and change interrupt detection to RISING
-      2012-06-05 - add gravity-compensated initial reference frame acceleration output
-                 - add 3D math helper file to DMP6 example sketch
-                 - add Euler output and Yaw/Pitch/Roll output formats
-      2012-06-04 - remove accel offset clearing for better results (thanks Sungon Lee)
-      2012-06-01 - fixed gyro sensitivity to be 2000 deg/sec instead of 250
-      2012-05-30 - basic DMP initialization working
-
-      2017-01-10 - [Rafael Bachmann] add servo output (needs low pass filter such as capacitor in parallel for pwm to work)
-      2017-01-30 - [Rafael Bachmann] modifications to run this sketch on the teensy 3.2
-      2017-01-30 - [Rafael Bachmann] removal of teapot output
-      2017-09-10 - [Rafael Bachmann] boil down to head tracker implementation with 0-calibration
-*/
-
 union int_bytes {
   uint8_t b[4];
   struct yp {
@@ -67,9 +18,8 @@ Servo yaw_servo;
 Servo pitch_servo;
 #endif
 
-#define CALIBRATION_PIN 14
-
 // Calibration variables
+#define CALIBRATION_PIN 14
 float yaw_offset = 0;
 float pitch_offset = 0;
 
@@ -272,6 +222,7 @@ void loop() {
     mpu.dmpGetGravity(&gravity, &q);
     mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
 
+    // TODO: use interrupt
     if (digitalRead(CALIBRATION_PIN) == LOW) {
       yaw_offset = ypr[0];
       pitch_offset = ypr[1];
@@ -280,7 +231,7 @@ void loop() {
     // yaw degrees
     // Add M_PI to get positive values (ypr[0] element of (-M_PI, M_PI)).
     // Angle in degree is ratio of reading to max reading * 180 where max reading: 2 * M_PI
-    int yaw_value = (int)180 - (ypr[0] + M_PI - yaw_offset) * 180 / (M_PI * 2);
+    int yaw_value = (int) 180 - (ypr[0] + M_PI - yaw_offset) * 180 / (M_PI * 2);
     /*yaw_value = yaw_value > 180.0 ? 180.0 : yaw_value;
       yaw_value = yaw_value < 0.0 ? 0.0 : yaw_value;*/
     // pitch degrees
@@ -302,9 +253,7 @@ void loop() {
     Serial.print("ypr\t");
     Serial.print(yaw_value);
     Serial.print("\t");
-    Serial.print(pitch_value);
-    Serial.print("\t");
-    Serial.println(ypr[2] * 180 / M_PI);
+    Serial.println(pitch_value);
 #endif
 
     int_bytes.yp.p = yaw_value;
