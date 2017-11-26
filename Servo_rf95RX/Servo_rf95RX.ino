27228fe6b86e4609907210570c4a021c4d02fcbc
Rafael Bachmann <mediumendian@gmail.com>
2017-11-26 19:16:39 +0100

    use manual servo control because libs conflict.

--- Servo_rf95RX/Servo_rf95RX.ino
+++ Servo_rf95RX/Servo_rf95RX.ino
@@ -1,37 +1,23 @@
-// Arduino9x_RX
-// -*- mode: C++ -*-
-// Example sketch showing how to create a simple messaging client (receiver)
-// with the RH_RF95 class. RH_RF95 class does not provide for addressing or
-// reliability, so you should only use RH_RF95 if you do not need the higher
-// level messaging abilities.
-// It is designed to work with the other example Arduino9x_TX
-
 union int_bytes {
-  uint8_t b[4];
-  struct yp {
-    uint16_t y;
-    uint16_t p;
-  } yp;
-} int_bytes;
+    uint8_t b[4];
+    struct yp {
+        uint16_t y;
+        uint16_t p;
+    }
+    yp;
+}
+int_bytes;
 
 #define DEBUG_OUT
 
-#include <Adafruit_PWMServoDriver.h>
-
-Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
-
-#define SERVOMIN  150 // this is the 'minimum' pulse length count (out of 4096)
-#define SERVOMAX  600 // this is the 'maximum' pulse length count (out of 4096)
-uint8_t servonum = 0;
-
-#include <SPI.h>
 #include <RH_RF95.h>
+#include <SPI.h>
 
 #define RFM95_CS 10
 #define RFM95_RST 9
 #define RFM95_INT 2
 
-// Change to 434.0 or other frequency, must match RX's freq!
+// Change to 868.0 or other frequency, must match RX's freq!
 #define RF95_FREQ 868.0
 
 // Singleton instance of the radio driver
@@ -40,73 +26,82 @@ RH_RF95 rf95(RFM95_CS, RFM95_INT);
 // Blinky on receipt
 #define LED_PIN 13
 
-void setup()
-{
-  pwm.begin();
-  pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
+const static uint8_t PITCH_SERVO_PIN = 5;
+const static uint8_t YAW_SERVO_PIN = 6;
 
-  yield();
+void setup() {
+    pinMode(LED_PIN, OUTPUT);
+    pinMode(RFM95_RST, OUTPUT);
+    digitalWrite(RFM95_RST, HIGH);
 
-  pinMode(LED_PIN, OUTPUT);
-  pinMode(RFM95_RST, OUTPUT);
-  digitalWrite(RFM95_RST, HIGH);
+    while (!Serial) { }
+    Serial.begin(9600);
 
-  while (!Serial);
-  Serial.begin(9600);
-  delay(100);
+    // manual reset
+    digitalWrite(RFM95_RST, LOW);
+    delay(10);
+    digitalWrite(RFM95_RST, HIGH);
+    delay(10);
 
-  Serial.println("Arduino LoRa RX Test!");
+    while (!rf95.init()) {
+        Serial.println("LoRa radio init failed");
+        while (1) { }
+    }
+    Serial.println("LoRa radio init OK!");
 
-  // manual reset
-  digitalWrite(RFM95_RST, LOW);
-  delay(10);
-  digitalWrite(RFM95_RST, HIGH);
-  delay(10);
+    // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+    if (!rf95.setFrequency(RF95_FREQ)) {
+        Serial.println("setFrequency failed");
+        while (1) { }
+    }
+    Serial.print("Set frequency to: ");
+    Serial.println(RF95_FREQ);
 
-  while (!rf95.init()) {
-    Serial.println("LoRa radio init failed");
-    while (1);
-  }
-  Serial.println("LoRa radio init OK!");
+    // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf =
+    // 128chips/symbol, CRC on
 
-  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
-  if (!rf95.setFrequency(RF95_FREQ)) {
-    Serial.println("setFrequency failed");
-    while (1);
-  }
-  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+    // The default transmitter power is 13dBm, using PA_BOOST.
+    // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter
+    // pin, then you can set transmitter powers from 5 to 23 dBm:
+    rf95.setTxPower(23, false);
 
-  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+    pinMode(PITCH_SERVO_PIN, OUTPUT);
+    pinMode(YAW_SERVO_PIN, OUTPUT);
 
-  // The default transmitter power is 13dBm, using PA_BOOST.
-  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
-  // you can set transmitter powers from 5 to 23 dBm:
-  rf95.setTxPower(23, false);
+    Serial.println("Headtracker Receiver initialised!");
 }
 
-void loop()
-{
-  if (rf95.available())
-  {
-    // Should be a message for us now
+void loop() {
+    if (rf95.available()) {
+        // Should be a message for us now
 
-    uint8_t len = sizeof(int_bytes.b);
+        uint8_t len = sizeof(int_bytes.b);
 
-    if (rf95.recv(int_bytes.b, &len))
-    {
-      pwm.setPWM(0, 0, (int)int_bytes.yp.y * 2.5 + 150);
-      pwm.setPWM(1, 0, (int)int_bytes.yp.p * 1.75 + 150);
-    }
-    else
-    {
-      Serial.println("Receive failed");
-    }
-  }
+        if (rf95.recv(int_bytes.b, & len)) {
+
+            servoPulse(PITCH_SERVO_PIN, int_bytes.yp.p);
+            servoPulse(YAW_SERVO_PIN, int_bytes.yp.y);
 
 #ifdef DEBUG_OUT
-  Serial.print(int_bytes.yp.y * 2.5 + 150);
-  Serial.print('\t');
-  Serial.println(int_bytes.yp.p * 1.75 + 150);
+            Serial.print(int_bytes.yp.y);
+            Serial.print(' ');
+            Serial.println(int_bytes.yp.p);
 #endif
+
+            /*
+              pwm.setPWM(0, 0, (int)int_bytes.yp.y * 2.5 + 150);
+              pwm.setPWM(1, 0, (int)int_bytes.yp.p * 1.75 + 150);
+             */
+        } else {
+            Serial.println("Receive failed");
+        }
+    }
 }
 
+void servoPulse(int servoPin, int myAngle) {
+    int pulseWidth = (myAngle * 11) + 500; // converts angle to microseconds
+    digitalWrite(servoPin, HIGH);      // set servo high.
+    delayMicroseconds(pulseWidth);     // wait a very small amount.
+    digitalWrite(servoPin, LOW);       // set servo low.
+    delay(10);                  // refresh cycle of typical servos.
+}
